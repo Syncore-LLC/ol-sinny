bba1cecdc6b8ca4882b13277cfa3970986b64b24
Shaun Chander <shaunchander@gmail.com>
2020-06-02 13:54:48 -0500

    ♻ change package.json

--- bot.ts
+++ bot.ts
@@ -1,19 +1,19 @@
 import 'dotenv/config'
-const { AkairoClient } = require('discord-akairo')
+import { AkairoClient, CommandHandler } from 'discord-akairo'
 
 class SyncoreBot extends AkairoClient {
   constructor() {
     super({ ownerID: '717195375731015732' })
 
-    this.commandHandler = new CommandHandler(this, {
-      directory: './commands/',
-      prefix: '!'
-    })
+    // this.commandHandler = new CommandHandler(this, {
+    //   directory: './commands/',
+    //   prefix: '!'
+    // })
 
-    this.commandHandler.loadAll()
+    // this.commandHandler.loadAll()
   }
 }
 
 const bot = new SyncoreBot()
 
-bot.login('NjQ5NDM4MzAzNTM1MzY2MjI2.XtWxig.5t46LbYbFjKlV7uhlJjWwMxGXi4')
+bot.login(process.env.BOT_TOKEN)
