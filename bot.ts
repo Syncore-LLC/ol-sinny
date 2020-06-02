4cf6d0a1aed4275b9f573abde89752a0faa4d7c5
Shaun Chander <shaunchander@gmail.com>
2020-06-02 03:12:31 -0500

    ♻ change to ESNext imports

--- bot.ts
+++ bot.ts
@@ -1,17 +1,1 @@
-const { AkairoClient } = require('discord-akairo')
-
-class MyClient extends AkairoClient {
-  constructor() {
-    super(
-      {
-        // Options for Akairo go here.
-      },
-      {
-        // Options for discord.js goes here.
-      }
-    )
-  }
-}
-
-const client = new MyClient()
-client.login('TOKEN')
+import { AkairoClient } from ''
